c19272337e4954092db4989024fa677675ac2a79
salehaTanveer <56721766+salehaTanveer@users.noreply.github.com>
2020-07-16 04:16:06 +0500

    upgrade react router dom to 6.0.0 alpha 2

--- src/App.js
+++ src/App.js
@@ -1,29 +1,27 @@
 import React from 'react';
 
-//import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import './App.css';
 
 
 export default function App() {
 
 return (
-  /*  <Router>
+   <Router>
       <Routes>
         <nav>
           <Link to="/"> Home</Link>
         </nav>
           <Route path="/" element={ <Home /> } />
       </Routes>
-    </Router>*/
-    <h1>hello world</h1>
+    </Router>
   );
 }
 
-/*function Home(){
+function Home(){
   return(
     <div>
       <h1> Welcome home</h1>
     </div>
   );
 }
-*/
